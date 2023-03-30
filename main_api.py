1426a1fa6add04c1a21e2742167c4cf4388d17c6
aboutmydreams <aboutmydreams@163.com>
2023-03-30 21:00:31 +0800

    feat: add get data from path

--- main_api.py
+++ main_api.py
@@ -22,7 +22,8 @@ class TrainInputs(Inputs):
     """
     json = [JsonSchema({
         'model_name': Union[str, int],
-        'urls': List[str],
+        'file_list': List[str],
+        'get_file_type': str, # "url" | "path"
     })]
 
 @app.route("/train", methods=["POST"])
@@ -35,16 +36,25 @@ def train():
         return jsonify({'errors': errors}), 400
 
     try:
-        urls = request.json["urls"]
+        file_list = request.json["file_list"]
         model_name = request.json["model_name"]
+        get_file_type = request.json["get_file_type"]
         index_path = "./training/models/{}.index".format(model_name)
         pkl_path = "./training/models/{}.pkl".format(model_name)
 
         trainingData = []
-        for url in urls:
-            r = requests.get(url)
-            if r.status_code == 200:
-                trainingData.append(r.text)
+        
+        if get_file_type == "url":
+            for url in file_list:
+                r = requests.get(url)
+                if r.status_code == 200:
+                    trainingData.append(r.text)
+        else:
+            for file_path in file_list:
+                with open(file_path, "r") as data_file:
+                    use_text = data_file.read()
+                trainingData.append(use_text)
+                data_file.close()
         if len(trainingData) < 1:
             return jsonify({"error": "No training data found"}), 400
         textSplitter = CharacterTextSplitter(chunk_size=2000, separator="\n")
@@ -56,6 +66,7 @@ def train():
         store.index = None
         with open(pkl_path, "wb") as f:
             pickle.dump(store, f)
+        # 如果需要下载 model 文件
         # return send_file("./training/models/{}.pkl".format(model_name), as_attachment=True)
         return jsonify({
                     "code": 0,
@@ -63,8 +74,7 @@ def train():
                     "data": {"model_name": model_name,
                              "size_type": "kb",
                              "pkl_model_size": get_file_size(pkl_path),
-                             "index_model_size": get_file_size(index_path)}
-                }), 200
+                             "index_model_size": get_file_size(index_path)}}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -81,37 +91,45 @@ class ChatInputs(Inputs):
 
 @app.route("/chat", methods=["POST"])
 def chat():
+    inputs = ChatInputs(request)
+    if not inputs.validate():
+        errors = []
+        for error in inputs.errors:
+            errors.append({'message': error.message})
+        return jsonify({'errors': errors}), 400
+    try:
+        model_name = request.json["model_name"]
+        question = request.json["question"]
+        history = request.json["history"]
 
-    model_name = request.json["model_name"]
-    question = request.json["question"]
-    history = request.json["history"]
+        index = faiss.read_index("./training/models/{}.index".format(model_name))
+        with open("./training/models/{}.pkl".format(model_name), "rb") as f:
+            store = pickle.load(f)
+        store.index = index
 
-    index = faiss.read_index("./training/models/{}.index".format(model_name))
-    with open("./training/models/{}.pkl".format(model_name), "rb") as f:
-        store = pickle.load(f)
-    store.index = index
+        with open("training/master.txt", "r") as f:
+            promptTemplate = f.read()
 
-    with open("training/master.txt", "r") as f:
-        promptTemplate = f.read()
-
-    prompt = Prompt(
-        template=promptTemplate, input_variables=["history", "context", "question"]
-    )
-    
-    llmChain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0.25,openai_api_key=os.environ["OPENAI_API_KEY"]))
-    
-    docs = store.similarity_search(question)
-    contexts = []
-    for i, doc in enumerate(docs):
-        contexts.append(f"Context {i}:\n{doc.page_content}")
-    answer = llmChain.predict(
-        question=question, context="\n\n".join(contexts), history=history
-    )
-    response = {
-        "answer": answer,
-        "history": history + [f"Human: {question}", f"Bot: {answer}"],
-    }
-    return jsonify(response)
+        prompt = Prompt(
+            template=promptTemplate, input_variables=["history", "context", "question"]
+        )
+        
+        llmChain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0.25,openai_api_key=os.environ["OPENAI_API_KEY"]))
+        
+        docs = store.similarity_search(question)
+        contexts = []
+        for i, doc in enumerate(docs):
+            contexts.append(f"Context {i}:\n{doc.page_content}")
+        answer = llmChain.predict(
+            question=question, context="\n\n".join(contexts), history=history
+        )
+        response = {
+            "answer": answer,
+            "history": history + [f"Human: {question}", f"Bot: {answer}"],
+        }
+        return jsonify(response)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == "__main__":
--- test/train_api.test.py
+++ test/train_api.test.py
@@ -1,16 +1,26 @@
 import requests
 import json
 
-url = "http://127.0.0.1:5000/train"
+def test_train_api(payload):
+    url = "http://127.0.0.1:5000/train"
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
+    return response.text
 
-payload = json.dumps({
-  "urls": ["https://cdn.vitae3.me/public-static/213113032213132120.1680148867987.txt"],
-  "model_name": "train_model_name",
+payload_url = json.dumps({
+    "file_list": ["https://cdn.vitae3.me/public-static/213113032213132120.1680148867987.txt"],
+    "model_name": "train_model_name",
+    "get_file_type": "url",
 })
-headers = {
-  'Content-Type': 'application/json'
-}
 
-response = requests.request("POST", url, headers=headers, data=payload)
+payload_path = json.dumps({
+    "file_list": ["./training/master.txt"],
+    "model_name": "train_model_name_path_test",
+    "get_file_type": "path",
+})
 
-print(response.text)
+test_train_api(payload_url)
+test_train_api(payload_path)
